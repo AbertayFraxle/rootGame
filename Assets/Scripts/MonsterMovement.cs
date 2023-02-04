5516c2ed90ec2a88c7cb5c83c65f4b0277092d48
howchen217 <59067683+howchen217@users.noreply.github.com>
2023-02-04 21:36:41 +0000

    add monster

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject player;
+    
+    private void Update()
+    {
+        var position = transform.position;
+        position = new Vector3(position.x + speed * Time.deltaTime, position.y, position.z);
+        transform.position = position;
+        
+        
+        //teleport back if behind player
+        if (transform.position.x > player.transform.position.x + GetScreenWidthInWorldDistance()) 
+        {
+            TeleportBack();
+        }
+    }
+
+    private float GetScreenWidthInWorldDistance()
+    {
+        return Camera.main.orthographicSize * Camera.main.aspect * 4;
+    }
+    
+    private void TeleportBack()
+    {
+        transform.position = new Vector3(player.transform.position.x - GetScreenWidthInWorldDistance(),
+            transform.position.y, transform.position.z);
+    }
+}
