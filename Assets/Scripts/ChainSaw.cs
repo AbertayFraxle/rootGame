49317a9291e0c05cb019dde4f8d5fabdbe796892
howchen217 <59067683+howchen217@users.noreply.github.com>
2023-02-05 12:16:46 +0000

    add leg cutting

--- Assets/Scripts/ChainSaw.cs
+++ Assets/Scripts/ChainSaw.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ChainSaw : MonoBehaviour
 {
     [SerializeField] private GameObject particle;
+    
+    [SerializeField] private List<GameObject> legs;
+
+    private bool isWaiting;
 
     private void Start()
     {
-       particle.SetActive(false); 
+       particle.SetActive(false);
+       isWaiting = false;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -17,6 +23,10 @@ public class ChainSaw : MonoBehaviour
         if (col.CompareTag("Player"))
         {
             particle.SetActive(true);
+            if (!isWaiting)
+            {
+                StartCoroutine(CutLeg());
+            }
         } 
     }
 
@@ -27,4 +37,31 @@ public class ChainSaw : MonoBehaviour
             particle.SetActive(false);
         } 
     }
+
+    /// <summary>
+    /// Cut leg and then wait three seconds until it can be done again. 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CutLeg()
+    {
+        isWaiting = true;
+        
+        //get random leg to remove
+        int legIndex = Random.Range(0, legs.Count);
+        var legToRemove = legs[legIndex];
+
+        //disable hinge joint
+        legToRemove.GetComponent<HingeJoint2D>().enabled = false;
+        
+        //unparent
+        legToRemove.transform.parent = null;
+        
+        //remove leg
+        legs.RemoveAt(legIndex);
+        
+        yield return new WaitForSecondsRealtime(3f);
+
+        isWaiting = false;
+    }
+    
 }
